f2d752a525726cc807d27dcc3d8fa7e00f45145f
Azibagiramani Kelvin Mansi <kelvinmansi@Kelvins-MacBook-Pro.local>
2023-03-08 18:05:26 +0100

    push

--- src/components/Elements/BaseFilter.tsx
+++ src/components/Elements/BaseFilter.tsx
@@ -1,7 +1,5 @@
 import { Flex, Button, Box, Input } from "@chakra-ui/react";
 
-import { SECONDARYCOLOR, PRIMARYCOLOR } from "@/utils/colors";
-
 import type { ChangeEventHandler, FC } from "react";
 
 const BaseFilter: FC<{
@@ -15,7 +13,6 @@ const BaseFilter: FC<{
         type="search"
         name="q"
         width="3xl"
-        bgColor={SECONDARYCOLOR}
         placeholder="Search by Name, Authors, Characters Name, Date Released, Publisher, Isbn and culture"
         value={"q"}
         size="md"
@@ -24,7 +21,6 @@ const BaseFilter: FC<{
       <Box>
         <Button
           loadingText="Fetching data ...."
-          bg={PRIMARYCOLOR}
           color={"white"}
           id="activity-button"
           isLoading={isLoading}
